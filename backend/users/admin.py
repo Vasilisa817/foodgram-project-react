a88c1a7476aec2f2a9af919b29daba6dec4f1bbe
Василиса Немоляева <mtk74@yandex.ru>
2022-10-14 01:35:21 +0500

    fix: delete AbstractUser

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -4,14 +4,14 @@ from backend.settings import EMPTY
 
 from users.models import Follow, User
 
-
+'''
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'username', 'email', 'first_name', 'last_name')
     search_fields = ('username', 'email')
     list_filter = ('username', 'email')
     empty_value_display = EMPTY
-
+'''
 
 @admin.register(Follow)
 class FollowAdmin(admin.ModelAdmin):
